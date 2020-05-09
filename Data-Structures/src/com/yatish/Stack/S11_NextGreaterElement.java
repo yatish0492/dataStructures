6b9bf590616d584262c79c5bf737ced35edd7c21
Yatish C S <ycs@hotels.com>
2020-05-10 03:21:49 +0530

    Updated stack program

--- Data-Structures/src/com/yatish/Stack/S11_NextGreaterElement.java
+++ Data-Structures/src/com/yatish/Stack/S11_NextGreaterElement.java
@@ -1,4 +1,46 @@
 package com.yatish.Stack;
 
+import java.util.Stack;
+
+/*
+    Next Greater element
+    --------------------
+    Input --> [13, 7, 6, 12]
+    Output --> Element    Next Greater Element
+                13      -->    -1     -> after '13' there is no greater element than '13' so we assign '-1'
+                7       -->     12    -> after '7' next greater element is '12'
+                6       -->     12    -> after '6' next greater element is '12'
+                12      -->     -1    -> after '12' there is no greater element than '12' so we assign '-1'
+
+
+
+ */
 public class S11_NextGreaterElement {
+
+    public void printNextGreaterElement(Integer[] input) {
+        Stack<Integer> stack = new Stack();
+        stack.push(input[0]);
+
+        for(int i = 1; i < input.length; i++) {
+            // The stack will have previous elements. so we will take each of them and check if the current element
+            // is greater than them and print them.
+            while(!stack.isEmpty() && input[i] > stack.peek()) {
+                System.out.println(stack.peek() + "   -->   " + input[i]);
+                stack.pop();
+            }
+            // we will add current element to stack as its next greater element will be found in next iteration.
+            stack.push(input[i]);
+        }
+
+        //Whatever elements left in  stack didn't find a greater value so we just print '-1' for them.
+        while(!stack.isEmpty()) {
+            System.out.println(stack.pop() + "   -->   -1");
+        }
+    }
+
+    public static void main(String[] args) {
+        S11_NextGreaterElement obj = new S11_NextGreaterElement();
+        Integer[] input = {13, 7, 6, 12};
+        obj.printNextGreaterElement(input);
+    }
 }
